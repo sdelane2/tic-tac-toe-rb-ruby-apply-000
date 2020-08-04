276729a234110fd4c6554e88c90ec770f9a85db6
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-04 20:44:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,14 +87,13 @@ def winner(board)
   end
 end
 def play(board)
-   until over?(board) 
-      turn(board)
-   end 
-   if won?(board)
-      winner(board) == "X" || winner(board) == "O" 
-      puts "Congratulations #{winner(board)}!" 
-   elsif draw?(board)
-      puts "Cat's Game!"
-    end
+  until over?(board)
+    turn(board) 
+  end
+  if won?(board)
+    winner(board) == "X" || winner(board) == "O"
+    puts "Congratulations #{winner(board)}!"
+  else draw?(board)
+    puts "Cat\'s Game!"
   end
 end
