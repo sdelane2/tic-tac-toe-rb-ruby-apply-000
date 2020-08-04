b1a2634516bf509b68441f48879281d47bc2a7da
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-04 20:21:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,12 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -28,7 +37,6 @@ end
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
-
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
@@ -49,5 +57,42 @@ def play(board)
   until counter == 9 
     turn(board)
     counter += 1
+  end
+end
+def won?(board)
+  WIN_COMBINATIONS.each do |win|
+  if win.all?{|sequence| board[sequence] == "X"}
+    return win
+    elsif win.all?{|sequence| board[sequence] == "O"}
+      return win
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+def draw?(board)
+  if !full?(board) && !won?(board)
+    return false
+  elsif won?(board)
+    return false 
+  elsif full?(board)
+    return true 
+  end
+end
+def over?(board)
+  if full?(board) || won?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
   end
 end
